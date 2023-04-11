ee89100708f54a3678d13886a9c1ead545fb2288
Rebecca168 <128460115+Rebecca168@users.noreply.github.com>
2023-04-11 14:48:47 +0800

    Add files via upload

---
+++ JAVA_練習1/src/JAVA_練習1/pkg1/範例1.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ *///
+
+package JAVA_練習1.pkg1;
+
+/**
+ *
+ * @author user
+ */
+public class  範例1{
+
+    /**
+     * @param args the command line arguments
+     */
+   
+    
+    public static void 顯示note(){
+         System.out.println("Hello! Hello! Hello!Charlie");
+    }
+    
+     public static void main(String[] args) {
+        // TODO code application logic here
+  
+    System.out.println("Hello! Charlie");
+ 
+    System.out.println("Hello! Rebecca");
+    
+    System.out.println("Hello! Hello!");
+    }
+    public static void 顯示NOTE2(){
+        System.out.println("Hello~Hello~Hello~ Charlie");
+    
+    }
+  
+
+}
---
+++ JAVA_練習1/src/JAVA_練習1/pkg1/範例1_1.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//主功能 程式執行起點
+//  public static void main(String[] args) { 
+//  System.out.println("Hello! Charlie");        }
+
+
+package JAVA_練習1.pkg1;
+
+/**
+ *
+ * @author user
+ */
+public class 範例1_1 {
+     public static void main(String[] args) {
+        // TODO code application logic here
+  
+    System.out.println("Hello! Charlie");
+ 
+    System.out.println("Hello! Rebecca");
+   範例1.顯示note();
+   範例1.顯示note();
+   範例1.顯示NOTE2();
+   範例1.顯示NOTE2();
+   
+}
+ public static void 計算() {
+     System.out.println("英文成績");
+     System.out.println(100);
+     System.out.println(100+10);
+     
+ }
+     
+}
+    
